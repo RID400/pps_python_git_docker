0cfb365ffca21613b042dd6618c6917231868f36
RID400 <1593589@alu.murciaeduca.es>
2024-01-24 17:49:59 +0100

    Actualización de ficheros

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print ("Hola Mundo")
+from flask import Flask, jsonify
+from bayeta import frotar
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return 'Hola, mundo'
+
+@app.route('/frotar/<int:n_frases>')
+def obtener_frases(n_frases):
+    frases = frotar(n_frases)
+    return jsonify({"frases_auspiciosas": frases})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- bayeta.py
+++ bayeta.py
@@ -1,1 +1,5 @@
-def frotar(n_frases: int = 1) -> list()
+from flask import Flask, jsonify
+
+def frotar(n_frases: int = 1) -> list:
+	frase = "Hola Mundo"
+	return [frase] * n_frases
